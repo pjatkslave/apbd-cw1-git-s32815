ad9abcffbbdc71965f5dabd26216c32b046965ab
pjatkslave <s32815@pjwstk.edu.pl>
2026-03-17 00:54:39 +0100

    fixing bugs, adding while loop for infinite slave work

--- Program.cs
+++ Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 Console.Writeline("Hello, your slave waiting for work");
 Console.Writeline("Write 'work' and pjatkslave will start to write a code for you");
-Console.Writeline("Write 'resr' and pjatkslave will rest");
+Console.Writeline("Write 'rest' and pjatkslave will rest");
 Slave mySlave = new Slave();
-string command = Console.ReadLine();
 
-if (command == "work")
+while (true)
 {
-    	mySlave.Work();
-}
-else if (command == "rest")
-{
-	mySlave.Rest();
-}
-else
-{
-Console.Writeline("error command");
+	string command = Console.ReadLine()
+	if (command == "exit") break;
+
+	if (command == "work")
+	{
+        if (mySlave.Energy > 0)
+        {
+            mySlave.Work();
+        }
+        else
+        {
+            Console.WriteLine("Error your slave is almost dead, give him some rest");
+        }
+    	}
+	else if (command == "rest")
+	{
+		mySlave.Rest();
+	}
+	else
+	{
+	Console.Writeline("error command");
+	}
 }
