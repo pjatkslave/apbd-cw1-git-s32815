d42bee5fabdba45dc2361ebdd5e6a0beee0820df
pjatkslave <s32815@pjwstk.edu.pl>
2026-03-17 00:37:13 +0100

    integreate slave class to main

--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
 ﻿using System;
-Console.Writeline("Hello, your slave waiting for work")
-Console.Writeline("Write 'work' and pjatkslave will start to write a code for you")
-string ans = Console.ReadLine();
+Console.Writeline("Hello, your slave waiting for work");
+Console.Writeline("Write 'work' and pjatkslave will start to write a code for you");
+Console.Writeline("Write 'resr' and pjatkslave will rest");
+Slave mySlave = new Slave();
+string command = Console.ReadLine();
+
+if (command == "work")
+{
+    	mySlave.Work();
+}
+else if (command == "rest")
+{
+	mySlave.Rest();
+}
+else
+{
+Console.Writeline("error command");
+}
