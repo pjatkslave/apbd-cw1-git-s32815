9ff5bec0a385cf695dd255799b891b4de8fa8d25
pjatkslave <s32815@pjwstk.edu.pl>
2026-03-17 02:46:20 +0100

    add punish method to Slave class

--- Slave.cs
+++ Slave.cs
@@ -21,4 +21,11 @@ public class Slave
         Hunger = 0;
         Console.WriteLine("You gave a cigarete to slave, now he is happy and doesnt have hunger");
     }
+    public void Punish()
+    {
+    	Energy -= 50;
+    	Hunger += 50;
+    	Console.WriteLine($"{Name} is punished, energy left: {Energy}%, hunger level: {Hunger}%, and he start to work harder, but you will not see the result of it, cause work of pjatkslave is pointless");
+    }
+	
 }
