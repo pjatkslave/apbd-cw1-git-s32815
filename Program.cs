4d6a4eaeac3c44ccab24e17d3dffc748977ccf4c
pjatkslave <s32815@pjwstk.edu.pl>
2026-03-17 04:14:06 +0100

    changed intro massage to simnulator

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-Console.WriteLine("Hello, your slave waiting for work");
+Console.WriteLine("Welcome to pjatkslave simulator");
 Console.WriteLine("Write 'work' and pjatkslave will start to write a code for you");
 Console.WriteLine("Write 'rest' and pjatkslave will rest");
 Console.WriteLine("Write 'feed' and you will give some food to pjatkslave");
