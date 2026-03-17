dbfb40a7efacc23732c19a487c836a7cf2798ad5
pjatkslave <s32815@pjwstk.edu.pl>
2026-03-17 02:09:41 +0100

    Add hunger scale and feed method to class Slave

--- Slave.cs
+++ Slave.cs
@@ -3,15 +3,22 @@ public class Slave
 {
     public string Name { get; set; } = "Alex";
     public int Energy { get; set; } = 100;
+    public int Hunger { get; set; } = 0;
 
-    public void Work()
+    	public void Work()
     {
         Energy -= 20;
+	Hunger += 20;
         Console.WriteLine($"{Name} is coding, energy left: {Energy}%");
     }
 	public void Rest()
     {
         Energy += 20;
         Console.WriteLine($"{Name} is watching tiktok instead of do sport, energy left: {Energy}%");
+    }
+    	public void Feed()
+    {
+        Hunger = 0;
+        Console.WriteLine("You gave a cigarete to slave, now he is happy and doesnt have hunger");
     }
 }
